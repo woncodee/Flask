c512882dbf9a865220e56c0dcdb0cfff45c46e50
woncodee <dnjswodus123@gmail.com>
2023-05-03 16:14:20 +0900

    Add files via upload

---
+++ Capstone_Flask/live_streaming.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+capture = cv2.VideoCapture(-1)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray,
+                                                  scaleFactor=1.1,
+                                                  minNeighbors=5,
+                                                  minSize=(30, 30),
+                                                  flags=cv2.CASCADE_SCALE_IMAGE)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+@app.route('/')
+def index():
+    return render_template('index4#2.html')             # index4#2.html의 형식대로 웹페이지를 보여줌
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') # 그림파일들을 쌓아서 보여줌
+
+if __name__ == "__main__":  # 웹사이트를 호스팅하여 접속자에게 보여주기 위한 부분
+   app.run(host="192.168.1.116", port = "8080")
+   # host는 현재 라즈베리파이의 내부 IP, port는 임의로 설정
+   # 해당 내부 IP와 port를 포트포워딩 해두면 외부에서도 접속가능
